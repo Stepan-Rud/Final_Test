eb53767d3ff5cbef405db3f1b71c429494b99d6f
RudStepan <1gooder2@gmail.com>
2023-09-27 21:09:43 +0700

    Add file READMY

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayFirst = new string[5] {"sunday", "munday", "sun", "help", "123" };
+string[] arraySecond = new string[arrayFirst.Length];
+PaddingWithThreeChar(arrayFirst, arraySecond);
+PrintArray(arraySecond);
+void PrintArray(string[] arr, string sep = " ")
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep}");
+    else Console.Write($"{arr[i]}");
+  }
+}
+void PaddingWithThreeChar(string[] arr, string[] newarr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+             newarr[count] = arr[i];
+             count++;
+        }
+    }
+}
